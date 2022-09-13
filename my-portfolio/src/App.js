4c634738cd32a593f6bde15646aae7e9064c09d3
raylu15742cs <raylucs15742@gmail.com>
2022-09-13 09:45:43 -0700

    add components to app

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,14 @@
-
+import Home from './Component/Home';
+import Project from './Component/Project';
+import Resume from './Component/Resume';
 
 function App() {
   return (
     <div className="App">
       <h1> Hi from App</h1>
+      <Home />
+      <Project />
+      <Resume />
     </div>
   );
 }
